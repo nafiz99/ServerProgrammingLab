6105cdfcc97471972fce11f543293f0a46381e66
nafiz99 <nafizimtiaz13@gmail.com>
2021-05-01 10:50:48 +0600

    completed globalVariables.js

---
+++ Lab_1_180042125/globalVariables.js
@@ -0,0 +1,20 @@
+//__dirname= path to the current directory
+
+const currentDirectory = __dirname;
+console.log(currentDirectory);
+
+//__filename= file name
+
+console.log(__filename);
+
+//require= function to use modules
+
+const fs = require("fs");
+
+//module= info about current module
+
+console.log(module);
+
+//process= info about where the program is being executed
+
+console.log(process);
