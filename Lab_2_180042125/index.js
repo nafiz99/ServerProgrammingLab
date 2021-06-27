e7449752ce44b46b10b61f63e4f1ed04f41759a2
Nafiz Imtiaz <nafizimtiaz13@gmail.com>
2021-06-27 20:11:57 +0600

    connected to mongodb

--- Lab_2_180042125/index.js
+++ Lab_2_180042125/index.js
@@ -1,6 +1,19 @@
 require(`dotenv`).config();
 const app=require(`./app`);
+const mongoose= require('mongoose');
 
+mongoose.connect(process.env.MONGOURL,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  
+}).then(()=>{
+    console.log('Database connected'
+    )}).catch((err)=>{
+        if(err){
+            console.log('Unable to connect to database')
+        }
+    })
 const PORT= process.env.PORT;
 
 app.listen(PORT, ()=> console.log(`Server has started on port ${PORT}`));
