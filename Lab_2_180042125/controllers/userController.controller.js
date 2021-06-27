0d80c7b280a407a806b3aab61f69061849ad8c51
Nafiz Imtiaz <nafizimtiaz13@gmail.com>
2021-06-27 13:09:53 +0600

    created controller file

---
+++ Lab_2_180042125/controllers/userController.controller.js
@@ -0,0 +1,5 @@
+const getLoginPage=(req,res)=>{
+    res.send("Login page from controller")
+};
+
+module.exports= {getLoginPage};
--- Lab_2_180042125/routes/userRoutes.routes.js
+++ Lab_2_180042125/routes/userRoutes.routes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const userController= require('../controllers/userController.controller');
 
-router.get('/login', (req,res)=>{
-  res.send("login page");
-})
+router.get('/login',userController.getLoginPage)
 
-module.exports = router
+module.exports = router;
