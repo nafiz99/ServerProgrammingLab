415b8bf0f6c8b70bce965a5d18a141926dc104d9
Nafiz Imtiaz <nafizimtiaz13@gmail.com>
2021-06-27 12:44:33 +0600

    created userRoute file

--- Lab_2_180042125/app.js
+++ Lab_2_180042125/app.js
@@ -2,11 +2,14 @@ const express= require('express');
 
 const cors=require('cors');
 const app=express();
+const userRoute=require('./routes/userRoutes.routes')
 
 app.use(cors());
+app.use(userRoute);
 
 app.get("/", (req, res) => {
     res.sendFile("index3.html", { root: "./views" });
   })
+
 
 module.exports= app;
