ac6e5dded2bc3bbce446c70e5b4c7099f0c2b024
Nafiz Imtiaz <nafizimtiaz13@gmail.com>
2021-06-28 15:48:35 +0600

    done styling

--- Lab_2_180042125/controllers/userController.controller.js
+++ Lab_2_180042125/controllers/userController.controller.js
@@ -15,6 +15,8 @@ const postRegisterPage=(req,res)=>{
     const name= req.body.name;
     const email= req.body.email;
     const password=req.body.password;
+    const retype=req.body.retype;
+    
     if(password.length<6 | !name | !email){
        return res.json({message: "Registration failed"});
     }
