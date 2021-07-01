efeaf2fc4ddc4af983387c5a3129489ef61c96a0
Nafiz Imtiaz <nafizimtiaz13@gmail.com>
2021-07-01 23:12:03 +0600

    connected database

--- Lab_2_180042125_solution/app.js
+++ Lab_2_180042125_solution/app.js
@@ -1,7 +1,18 @@
+require('dotenv').config();
 const express= require("express");
 const app= express();
 const session= require('express-session');
 const flash= require('connect-flash');
+const mongoose= require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+    console.log("Connected to database")
+})
+.catch((error)=>{
+    console.log(error);
+});
+
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -15,6 +26,7 @@ app.use(flash());
 
 const indexRoutes= require("./routes/index.routes");
 const userRoutes= require("./routes/user.routes");
+const { Mongoose } = require("mongoose");
 
 app.use(express.urlencoded({extended:false}));
 
