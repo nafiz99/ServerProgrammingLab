8f7fe1861103dafd77ec895d464eaba2e4de6609
Nafiz Imtiaz <nafizimtiaz13@gmail.com>
2021-07-02 11:06:34 +0600

    done

--- Lab_2_180042125/app.js
+++ Lab_2_180042125/app.js
@@ -1,11 +1,11 @@
 const express= require('express');
 const bodyParser= require('body-parser');
 
-const cors=require('cors');
+
 const app=express();
 const userRoute=require('./routes/userRoutes.routes')
 
-app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
