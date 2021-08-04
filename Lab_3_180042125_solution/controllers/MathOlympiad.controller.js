a766e023e2a33b64b0030a29a5e66f8668319887
Nafiz Imtiaz <nafizimtiaz13@gmail.com>
2021-08-04 09:31:02 +0600

    created MathOlympiadController

---
+++ Lab_3_180042125_solution/controllers/MathOlympiad.controller.js
@@ -0,0 +1,20 @@
+const getMO= (req,res)=>{
+    res.render('math-olympiad/register.ejs');
+
+};
+
+const postMO=(req,res)=>{
+    res.render('math-olympiad/register.ejs');
+};
+
+const getMOList=(req,res)=>{
+    res.render('math-olympiad/list.ejs');
+};
+
+const deleteMO= (reqq,res)=>{
+    const id= req.params.id;
+    console.log(id);
+    res.render('math-olympiad/list.ejs')
+};
+
+module.exports={getMO,postMO,getMOList,deleteMO}
