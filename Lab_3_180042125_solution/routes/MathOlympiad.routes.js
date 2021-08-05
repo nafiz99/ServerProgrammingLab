13f5812006ed4146213516455ad3d246ea9c47cc
Nafiz Imtiaz <nafizimtiaz13@gmail.com>
2021-08-05 23:48:49 +0600

    paymentDoneMO done

--- Lab_3_180042125_solution/routes/MathOlympiad.routes.js
+++ Lab_3_180042125_solution/routes/MathOlympiad.routes.js
@@ -3,7 +3,7 @@ const router= express.Router();
 
 const {ensureAuthenticated, addUserData}=require('../middlewares/auth.middleware');
 
-const {getMO, postMO, getMOList, deleteMO}= require('./../controllers/MathOlympiad.controller');
+const {getMO, postMO, getMOList, deleteMO, paymentDoneMO}= require('./../controllers/MathOlympiad.controller');
 
 router.get('/register',ensureAuthenticated,addUserData, getMO);
 router.post('/register',ensureAuthenticated,addUserData, postMO);
