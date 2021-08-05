1e1f3ca84775b731aee19c4af3dd7e67215f62c4
Nafiz Imtiaz <nafizimtiaz13@gmail.com>
2021-08-05 21:19:34 +0600

    changed postMO

--- Lab_3_180042125_solution/controllers/MathOlympiad.controller.js
+++ Lab_3_180042125_solution/controllers/MathOlympiad.controller.js
@@ -5,65 +5,74 @@ const getMO= (req,res)=>{
 
 };
 
-const postMO=(req,res)=>{
-    const {name,category, contact, email, institution, tshirt}= req.body;
-    console.log(name);
-    console.log(contact);
-    console.log(email);
+const postMO = (req, res) => {
+    const { name, category, contact, email, institution, tshirt } = req.body;
     console.log(institution);
-    console.log(category);
-    console.log(tshirt);
-    let regfee=0;
-    if(catefory="School"){
-        regfee=250;
-    } else if(category="College"){
-        regfee=400;
-
+    let registrationFee = 0;
+    if (category == "School") {
+      registrationFee = 250;
+    } else if (category == "College") {
+      registrationFee = 400;
     } else {
-        regfee=500;
+      registrationFee = 500;
     }
-    const total= regfee;
-    const paid=0;
-    const selected= false;
-
-    let error=""
-
-    MathOlympiad.findOne({name:name, contact:contact}).then((participant)=>{
-        if(participant){
-            error="Participant with this name and contact number already exists"
-            console.log(error);
-            res.redirect("register")
-        } else{
-            const participant= new MathOlympiad({
-                name,
-                category,
-                contact,
-                email,
-                institution,
-                paid,
-                total,
-                selected,
-                tshirt
-            });
-            participant.save().then(()=>{
-                error= "Participant has been added successfully"
-                console.log(error);
-                req.flash("error", error)
-                res.redirect("register")
-            })
-            .catch(()=>{
-                error="An unexpected error occured while registering participant"
-            })
-        }
-    })
-    
-    res.render("math-olympiad/register.ejs")
-    
-};
-
-const getMOList=(req,res)=>{
-    res.render('math-olympiad/list.ejs');
-};
+    const total = registrationFee;
+    const paid = 0;
+    const selected = false;
+    let error = "";
+  
+    MathOlympiad.findOne({ name: name, contact: contact }).then((participant) => {
+      if (participant) {
+        error = "Participant with same name and contact exists";
+  
+        req.flash("error", error);
+        res.redirect("/MathOlympiad/register");
+      } else {
+        const participant = new MathOlympiad({
+          name,
+          category,
+          contact,
+          email,
+          institution,
+          paid,
+          total,
+          selected,
+          tshirt,
+        });
+        participant
+          .save()
+          .then(() => {
+            error = "Participant has been registered successfully!!";
+            req.flash("error", error);
+            res.redirect("/MathOlympiad/register");
+          })
+          .catch(() => {
+            error = "Unexpected error";
+            req.flash("error", error);
+            res.redirect("/MathOlympiad/register");
+          });
+      }
+    });
+  };
+const getMOList = (req, res) => {
+    let all_participant = [];
+    let error = "";
+    MathOlympiad.find()
+      .then((data) => {
+        all_participant = data;
+        res.render("math-olympiad/list.ejs", {
+          error: req.flash("error"),
+          participants: all_participant,
+        });
+      })
+      .catch(() => {
+        error = "Failed to fetch participants";
+        res.render("math-olympiad/list.ejs", {
+          error: req.flash("error", error),
+          participants: all_participant,
+        });
+      });
+  };
 
 const deleteMO= (req,res)=>{
     const id= req.params.id;
