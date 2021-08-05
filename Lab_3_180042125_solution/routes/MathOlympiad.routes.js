36a248481aca7cb6d9cc036546769c07d401e7af
Nafiz Imtiaz <nafizimtiaz13@gmail.com>
2021-08-05 23:37:38 +0600

    paymentdoneMO

--- Lab_3_180042125_solution/routes/MathOlympiad.routes.js
+++ Lab_3_180042125_solution/routes/MathOlympiad.routes.js
@@ -9,5 +9,6 @@ router.get('/register',ensureAuthenticated,addUserData, getMO);
 router.post('/register',ensureAuthenticated,addUserData, postMO);
 router.get('/list', ensureAuthenticated, addUserData, getMOList);
 router.get('/delete/:id',ensureAuthenticated,addUserData, deleteMO);
+router.get('/paymentDone/:id',ensureAuthenticated,addUserData,paymentDoneMO);
 
 module.exports= router;
